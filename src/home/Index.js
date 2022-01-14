4f78aaa412a5e1b29bf1e1c46e37ade55ebe3471
Moh Zulfiqar Naufal Maulana <mznaufalmaulana@gmail.com>
2022-01-14 16:09:00 +0700

    perbaikan slick

--- src/home/Index.js
+++ src/home/Index.js
@@ -3,20 +3,22 @@ import React from "react";
 // import { Carousel } from "react-responsive-carousel";
 import Carousel from "react-responsive-carousel/lib/js/components/Carousel/index";
 import "react-responsive-carousel/lib/styles/carousel.css";
-import MultiCarousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import MultiCarousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import API from "../services";
+
+require("../home/style.css");
 
 class Index extends React.Component {
   state = {
     promoHeadline: [],
     promo: [],
-    collection: []
+    collection: [],
   };
 
   componentDidMount() {
@@ -32,11 +34,11 @@ class Index extends React.Component {
       }
     });
 
-    API.get(`product/related/8`).then((result) => {    
-      if (result.message === "success") {        
+    API.get(`product/related/8`).then((result) => {
+      if (result.message === "success") {
         this.setState({ collection: result.data });
-      }      
-    })  
+      }
+    });
   }
 
   render() {
@@ -55,58 +57,57 @@ class Index extends React.Component {
       emulateTouch: true,
       selectedItem: promoHeadline.length,
       interval: 10000,
-      transitionTime: 2000,      
+      transitionTime: 2000,
     };
 
     const responsive = {
       desktop: {
         breakpoint: { max: 3000, min: 1024 },
         items: 3,
-        slidesToSlide: 3 // optional, default to 1.
+        slidesToSlide: 3, // optional, default to 1.
       },
       tablet: {
         breakpoint: { max: 1024, min: 464 },
         items: 2,
-        slidesToSlide: 2 // optional, default to 1.
+        slidesToSlide: 2, // optional, default to 1.
       },
       mobile: {
         breakpoint: { max: 464, min: 0 },
         items: 1,
-        slidesToSlide: 1 // optional, default to 1.
-      }
+        slidesToSlide: 1, // optional, default to 1.
+      },
     };
 
     const settings = {
       dots: true,
-      infinite: true,      
+      infinite: true,
       autoplay: true,
       speed: 1000,
       autoplaySpeed: 10000,
       cssEase: "linear",
       slidesToShow: 1,
-      slidesToScroll: 1,      
+      slidesToScroll: 1,
       fade: true,
-      pauseOnHover: true      
+      pauseOnHover: true,
     };
 
-    return (      
+    return (
       <div>
-        <div className="mar-top-2">          
-            <Slider {...settings} >
-              {this.state.promoHeadline.map((promoHead) => (
-                 <a href={promoHead.link} key={promoHead.id}>
-                  <div
-                    className="s-holder wow fadeInLeft each-fade"
-                    data-wow-delay="0.4s"         
-                          
-                  >
-                    {/* <a href={promoHead.link}> */}
-                      <img src={`${API.urlStorage}${promoHead.photo_name}`}/>
-                    {/* </a> */}
-                  </div>                                
-                 </a>                        
-              ))}
-            </Slider>                      
+        <div className="mar-top-2">
+          <Slider {...settings}>
+            {this.state.promoHeadline.map((promoHead, index) => (
+              <a href={promoHead.link} key={index}>
+                <div
+                  className="s-holder wow fadeInLeft each-fade"
+                  data-wow-delay="0.4s"
+                >
+                  {/* <a href={promoHead.link}> */}
+                  <img src={`${API.urlStorage}${promoHead.photo_name}`} />
+                  {/* </a> */}
+                </div>
+              </a>
+            ))}
+          </Slider>
         </div>
 
         <main id="mt-main">
@@ -114,47 +115,76 @@ class Index extends React.Component {
             <div className="row">
               <div className="col-xs-12">
                 <div className="banner-frame pad-bot-1">
-                  { this.state.promo.map((promo, index) => {
+                  {this.state.promo.map((promo, index) => {
                     const order = [0];
-                    return order.includes(index) ? (                                           
-                      <div className="banner-1 wow fadeInLeft image-zoom-promo" data-wow-delay="0.4s" key={promo.id}>
+                    return order.includes(index) ? (
+                      <div
+                        className="banner-1 wow fadeInLeft image-zoom-promo"
+                        data-wow-delay="0.4s"
+                        key={promo.id}
+                      >
                         <a href={promo.link}>
-                          <img alt="image description" src={`${API.urlStorage}${promo.photo_name}`}/>                        
+                          <img
+                            alt="image description"
+                            src={`${API.urlStorage}${promo.photo_name}`}
+                          />
                         </a>
-                      </div>   
-                    ):null;
-                  })}                                      
-                  
-                  <div className="banner-box first">   
-                    { this.state.promo.map((promo, index) => {
-                      const order = [1,2];
-                      return order.includes(index) ? (                                           
-                        <div className={`banner-${index+1} wow fadeInUp image-zoom-promo`} data-wow-delay="0.4s" key={promo.id}>
+                      </div>
+                    ) : null;
+                  })}
+
+                  <div className="banner-box first">
+                    {this.state.promo.map((promo, index) => {
+                      const order = [1, 2];
+                      return order.includes(index) ? (
+                        <div
+                          className={`banner-${
+                            index + 1
+                          } wow fadeInUp image-zoom-promo`}
+                          data-wow-delay="0.4s"
+                          key={promo.id}
+                        >
                           <a href={promo.link}>
-                            <img alt="image description" src={`${API.urlStorage}${promo.photo_name}`}/>                   
+                            <img
+                              alt="image description"
+                              src={`${API.urlStorage}${promo.photo_name}`}
+                            />
                           </a>
-                        </div>   
-                      ):null;
-                    })}                                              
-                  </div>                  
-                  
-                  { this.state.promo.map((promo, index) => {
+                        </div>
+                      ) : null;
+                    })}
+                  </div>
+
+                  {this.state.promo.map((promo, index) => {
                     const order = [3];
-                    return order.includes(index) ? (                                           
-                      <div className={`banner-${index+1} wow fadeInRight image-zoom-promo`} data-wow-delay="0.4s" key={promo.id}>
+                    return order.includes(index) ? (
+                      <div
+                        className={`banner-${
+                          index + 1
+                        } wow fadeInRight image-zoom-promo`}
+                        data-wow-delay="0.4s"
+                        key={promo.id}
+                      >
                         <a href={promo.link}>
-                          <img alt="image description" src={`${API.urlStorage}${promo.photo_name}`}/>                        
+                          <img
+                            alt="image description"
+                            src={`${API.urlStorage}${promo.photo_name}`}
+                          />
                         </a>
-                      </div>   
-                    ):null;
-                  })}                              
-                </div>                    
-                <br /><br />       
+                      </div>
+                    ) : null;
+                  })}
+                </div>
+                <br />
+                <br />
               </div>
             </div>
           </div>
-          
-          <div className="mt-bestseller text-center wow fadeInUp" data-wow-delay="0.4s">
+
+          <div
+            className="mt-bestseller text-center wow fadeInUp"
+            data-wow-delay="0.4s"
+          >
             <div className="container">
               <div className="row">
                 <div className="col-xs-12 mt-heading text-uppercase">
@@ -163,35 +193,34 @@ class Index extends React.Component {
                 </div>
               </div>
               <div className="row">
-                <div className="col-md-12 z-index">                  
-                  <MultiCarousel                                                       
-                    ssr={true}                      
+                <div className="col-md-12 z-index">
+                  <MultiCarousel
+                    ssr={true}
                     deviceType={this.props.deviceType}
                     itemClass="image-item"
                     responsive={responsive}
                   >
-                    {this.state.collection.map((collection) => (          
-                      <div className="image-zoom-collection collection-name wow fadeInUp" 
-                        data-wow-delay="0.4s" 
+                    {this.state.collection.map((collection) => (
+                      <div
+                        className="image-zoom-collection collection-name wow fadeInUp"
+                        data-wow-delay="0.4s"
                         key={collection.id}
                       >
                         <a href={`/product/detail?product=${collection.id}`}>
-                          <img 
+                          <img
                             draggable={false}
                             src={`${API.urlStorage}${collection.photo_name}`}
-                          />                                                   
-                          <strong>                              
-                            {collection.product_name}                              
-                          </strong>                          
-                        </a> 
+                          />
+                          <strong>{collection.product_name}</strong>
+                        </a>
                       </div>
                     ))}
-                  </MultiCarousel>                  
+                  </MultiCarousel>
                 </div>
               </div>
             </div>
           </div>
-        </main>        
+        </main>
       </div>
     );
   }
